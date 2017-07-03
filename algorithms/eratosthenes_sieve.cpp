a7e76517e9bcd7675aa54c8c9e9769567ec1651d
Mantas Miksys <man.miksys@gmail.com>
2017-07-03 23:53:24 +0100

    Add Eratosthenes Sieve algorithm

---
+++ algorithms/eratosthenes_sieve.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+vector<bool> sieve (int n)
+{
+    vector<bool> prime(n+1, true);
+    prime[0] = false;
+    prime[1] = false;
+
+    for (auto i = 0; i < sqrt(n); ++i) {
+        if (prime[i]) {
+            for (auto j = i * i; j < n; j+=i) {
+                prime[j] = false;
+            }
+        }
+    }
+
+
+    return prime;
+}
+
+int main() {
+    auto prime = sieve(5);
+
+    cout << prime[2] << endl;
+
+    cout << prime[4] << endl;
+
+    return 0;
+}
