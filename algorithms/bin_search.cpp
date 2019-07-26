f8671a66c2b0b743b859c5ffa9af63180591eb9b
Mantas Miksys <mmiksys@uber.com>
2019-07-26 17:40:47 +0300

    Add algorithm for finding all paths

--- algorithms/bin_search.cpp
+++ algorithms/bin_search.cpp
@@ -1,27 +1,29 @@
-#include <vector>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-int binSearch(vector<int> v, int i){
-	int f = 0, t = v.size()-1, m;
-	while(f<=t){
-		m = (f+t)/2;
-		if(v[m]==i)
-			return m;
-		else if(v[m]>i)
-			f = m + 1;
-		else
-			t = m - 1;
-	}
-	return -1;
+// smallest index matching the specified value
+int binSearchSmallest(const vector<int>& v, int x) { return -1; }
+
+int binSearch(const vector<int>& v, int x) {
+  int i = 0, j = v.size() - 1, m;
+  while (i < j) {
+    m = (i + j) / 2;
+    if (v[m] == i)
+      return m;
+    else if (v[m] > i)
+      i = m + 1;
+    else
+      j = m - 1;
+  }
+  return (v[i] == x) ? i : -1;
 }
 
+int main() {
+  vector<int> v = {1, 3, 4, 6, 8, 9};
+  cout << binSearch(v, 4) << endl;
+  cout << binSearch(v, 2) << endl;
 
-int main(){
-	vector<int> v = {1, 3, 4, 6, 8, 9};
-	cout << binSearch(v, 4) << endl;
-	cout << binSearch(v, 2) << endl;
-	
-	return 0;
+  return 0;
 }
--- algorithms/topological_sort.cpp
+++ algorithms/topological_sort.cpp
@@ -1,40 +1,40 @@
 #include <iostream>
-#include <vector>
 #include <stack>
+#include <vector>
 
 using namespace std;
 
 // time complexity O(V + E)
 
-void topologicalSortUtil(const vector<vector<int>> & adj, vector<bool> & visited, stack<int> & s, int from) {
-    visited[from] = true;
-    for(int n : adj[from]) {
-        if(!visited[n])
-            topologicalSortUtil(adj, visited, s, n);
-    }
-    s.push(from);
+void topologicalSortUtil(const vector<vector<int>>& adj, vector<bool>& visited,
+                         stack<int>& s, int from) {
+  visited[from] = true;
+  for (int n : adj[from]) {
+    if (!visited[n]) topologicalSortUtil(adj, visited, s, n);
+  }
+  s.push(from);
 }
 
-stack<int> topologicalSort(const vector<vector<int>> & adj) {
-    stack<int> s;
-    vector<bool> visited(adj.size(), false);
-    for(int i = 0; i < visited.size(); ++i) {
-        if(!visited[i])
-            topologicalSortUtil(adj, visited, s, i);
-    }
-    return s;
+stack<int> topologicalSort(const vector<vector<int>>& adj) {
+  stack<int> s;
+  vector<bool> visited(adj.size(), false);
+  for (int i = 0; i < visited.size(); ++i) {
+    if (!visited[i]) topologicalSortUtil(adj, visited, s, i);
+  }
+  return s;
 }
 
 int main() {
-    vector<vector<int>> adj(6);
-    adj[5].insert(adj[5].end(), {0, 2});
-    adj[4].insert(adj[4].end(), {0, 1});
-    adj[2].push_back(3);
-    adj[3].push_back(1);
-    stack<int> s = topologicalSort(adj);
-    while(!s.empty()) {
-        cout << s.top() << " ";
-        s.pop();
-    }
-    cout << endl;
+  vector<vector<int>> adj{{2}, {2}, {}};
+  // vector<vector<int>> adj(6);
+  // adj[5].insert(adj[5].end(), {0, 2});
+  // adj[4].insert(adj[4].end(), {0, 1});
+  // adj[2].push_back(3);
+  // adj[3].push_back(1);
+  stack<int> s = topologicalSort(adj);
+  while (!s.empty()) {
+    cout << s.top() << " ";
+    s.pop();
+  }
+  cout << endl;
 }
