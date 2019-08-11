8926b337cd872734950f20339e43ed630c0f5549
Mantas Miksys <mmiksys@uber.com>
2019-08-11 15:38:37 +0300

    Use vector for union find

--- data_structures/union_find_set.h
+++ data_structures/union_find_set.h
@@ -1,36 +1,34 @@
+#include <numeric>
+#include <vector>
+
+using namespace std;
+
 // this is actually weighted union which gives O(log(n))
 class UnionFindSet {
-private:
-    int root(int a) {
-        while(arr[a] != a) {
-            // This line make root O(log*(N)) which is pretty much const
-            arr[a] = arr[arr[a]];
-            a = arr[a];
-        }
-        return a;
-    }
-    int * arr;
-    int * size;
-public:
-    UnionFindSet(int N) : arr(new int[N]), size(new int[N]) {
-        for(int i = 0; i < N; ++i) {
-            arr[i] = i;
-            size[i] = 1;
-        }
-    }
+ private:
+  vector<int> arr;
+  vector<int> size;
 
-    void makeUnion(int a, int b) {
-        int rootA = root(a);
-        int rootB = root(b);
-        if(rootA == rootB)
-            return;
-        int largerRoot = (size[rootA] > size[rootB]) ? rootA : rootB;
-        int smallerRoot = (size[rootA] <= size[rootB]) ? rootA : rootB;
-        arr[smallerRoot] = largerRoot;
-        size[largerRoot]+= size[smallerRoot];
+  int root(int a) {
+    while (arr[a] != a) {
+      // This line make root O(log*(N)) which is pretty much const
+      arr[a] = arr[arr[a]];
+      a = arr[a];
     }
+    return a;
+  }
 
-    bool find(int a, int b) {
-        return root(a) == root(b);
-    }
+ public:
+  UnionFindSet(int N) : arr(N), size(N, 1) { iota(arr.begin(), arr.end(), 0); }
+
+  void makeUnion(int a, int b) {
+    int rootA = root(a), rootB = root(b);
+    if (rootA == rootB) return;
+    int largerRoot = (size[rootA] > size[rootB]) ? rootA : rootB;
+    int smallerRoot = (size[rootA] <= size[rootB]) ? rootA : rootB;
+    arr[smallerRoot] = largerRoot;
+    size[largerRoot] += size[smallerRoot];
+  }
+
+  bool find(int a, int b) { return root(a) == root(b); }
 };
